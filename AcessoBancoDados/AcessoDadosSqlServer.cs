c021267ade1d687a9947fdf3c037332f41d2633d
Italo Santos <itallo_@Outlook.com>
2018-01-14 14:25:02 -0200

    commit do banco de dados

---
+++ AcessoBancoDados/AcessoDadosSqlServer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//classes para manipulação de dados
+using System.Data;
+using System.Data.SqlClient;
+using AcessoBancoDados.Properties;
+
+namespace AcessoBancoDados
+{
+    public class AcessoDadosSqlServer
+    {
+        //criar conexao
+        private SqlConnection CriarConexao()
+        {
+            return new SqlConnection(Settings.Default.stringConexao);
+        }
+
+        //parametros que vão para o banco
+        private SqlParameterCollection sqlParameterCollection = new SqlCommand().Parameters;
+
+        public void LimparParametros()
+        {
+            sqlParameterCollection.Clear();
+        }
+
+        public void AdicionarParametros(string nomeParametro, object valorParametro)
+        {
+            sqlParameterCollection.Add(new SqlParameter(nomeParametro, valorParametro));
+        }
+
+        //persitir- inserir - alterar - excluir
+        public object ExecutarManipular(CommandType commandType, string nomeStoredProcedureOuTextoSql)
+        {
+            try
+            {
+                //abrindo conexão
+                SqlConnection sqlConnection = CriarConexao();
+                //abrir conexão
+                sqlConnection.Open();
+                //criando comando que vai levar a informação para o banco.
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                //colocando as coisas dentro do comando
+                sqlCommand.CommandType = commandType;
+                sqlCommand.CommandText = nomeStoredProcedureOuTextoSql;
+                sqlCommand.CommandTimeout = 7200;//em segundos(2 horas)
+
+                //adicionar parametros do commando
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(sqlParameter.ParameterName, sqlParameter.Value));
+                }
+                //executando o comando, mandando o commando ir até o banco de dados
+                //ExecuteScalar() vai no banco e volta  1 coluna e 1 linha
+                return sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //cosultar registro do banco de dados
+        public DataTable ExecutarConsulta(CommandType commandType, string nomeStoredProcedureOuTextoSql)
+        {
+            try
+            {
+                //abrindo conexão
+                SqlConnection sqlConnection = CriarConexao();
+                //abrir conexão
+                sqlConnection.Open();
+                //criando comando que vai levar a informação para o banco.
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                //colocando as coisas dentro do comando
+                sqlCommand.CommandType = commandType;
+                sqlCommand.CommandText = nomeStoredProcedureOuTextoSql;
+                sqlCommand.CommandTimeout = 7200;//em segundos(2 horas)
+
+                //adicionar parametros do commando
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(sqlParameter.ParameterName, sqlParameter.Value));
+                }
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                //datatable = tabela de dados vazias onde vou volocar os dados dos banco
+                DataTable dataTable = new DataTable();
+                //mandar os commandos ate o banco buscar os dados para preencher a datatable
+                sqlDataAdapter.Fill(dataTable);
+
+                return dataTable;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+    }
+}
