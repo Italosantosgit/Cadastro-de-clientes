d5b4bcad4d9a0f63b3f0f76b876ddb0ba10bd3f8
Italo Santos <itallo_@Outlook.com>
2018-01-14 14:21:33 -0200

    commit do objeto transferencia

---
+++ ObjetoTransferencia/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ObjetoTransferencia
+{
+    public class Cliente
+    {
+        //variavel
+        public int IdCliente{ get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Boolean Sexo { get; set; }
+        public decimal LimiteCompra { get; set; }
+
+    }
+}
